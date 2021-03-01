5a753b716db57fd404b83e8c9b4ceac0efaf2acf
nmadev <60518226+nmadev@users.noreply.github.com>
2021-02-28 20:22:15 -0500

    Create reverse.c

---
+++ 02_29_2021/reverse.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int max_consecutive_ones(int *arr, int size) {
+    int max_ones = 0;
+    int curr_ones = 0;
+    int i = 0;
+    while (i < size) {
+        if (arr[i] == 1) {
+            curr_ones += 1;
+        }
+        else {
+            if (curr_ones > max_ones) {
+                max_ones = curr_ones;
+            }
+            curr_ones = 0;
+        }
+        i += 1;
+        
+    }
+    if (curr_ones > max_ones) {
+        return curr_ones;
+    }
+    else {
+        return max_ones;
+    }
+    
+}
+
+int main(void) {
+    int arr[] = {1, 1, 0, 1, 1, 1};
+    int size = 6;
+    int max_ones = max_consecutive_ones(arr, size);
+    printf("%i\n", max_ones);
+    return 0;
+}
+
+// Credit: Shayna Sragovicz
