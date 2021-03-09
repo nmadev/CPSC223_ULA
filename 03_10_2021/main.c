efb46b34c531b576e7147cfca64b4a3c0e3fd9d2
Sragovicz Shayna <sls248@zoo.cs.yale.edu>
2021-03-09 18:34:43 -0500

    added stuff in main

--- 03_10_2021/main.c
+++ 03_10_2021/main.c
@@ -5,12 +5,27 @@
 #include "movie_theater.h"
 
 int main(void) {
-
+    int num_movies = 100;
     // list of movies
+    char *movie_names[num_movies] = {"Harry Potter and the Sorcerer's Stone", "Harry Potter and the Chamber of Secrets"}
+    movie **list_of_movies = malloc(sizeof(movie *) * num_movies);
     // for every movie in list of movies
+    for (size_t i = 0; i < num_movies; i++) {
         // create movie struct
+        movie *m = create_movie(movie_names[i]);
         // add movie struct to list of movies
+        list_of_movies[i] = m;
+    }
+    
+    // get total tickets sold O(n)
+    // WHAT WE HAVE
+    size_t total_sold = 0;
+    for (int i = 0; i < num_movies; i++) {
+        size_t tickets_sold = list_of_movies[i].tickets_sold;
+        total_sold += tickets_sold;
+    }
+    printf("Total tickets sold: %li\n", total_sold);
 
-    // get total tickets sold O(n) -> O(1)
-
+    // WHAT WE WANT
+    // printf("Total tickets sold: %li\n", get_total_tickets(mt));
 }
