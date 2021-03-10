598070ae35ec9cb1e594af9caa0b8f27546feaf6
Sragovicz Shayna <sls248@zoo.cs.yale.edu>
2021-03-09 21:27:57 -0500

    different style

--- 03_10_2021/movie_theater.c
+++ 03_10_2021/movie_theater.c
@@ -5,8 +5,7 @@
 #include "movie_theater.h"
 #include "movies.h"
 
-struct theater
-{
+struct theater {
     movie **all_movies;
     size_t size;
     size_t capacity;
@@ -14,8 +13,7 @@ struct theater
 };
 
 // create a theater
-theater *theater_create() 
-{
+theater *theater_create() {
     theater *t = malloc(sizeof(theater));
     if (t != NULL) {
         t->size = 0;
@@ -25,8 +23,7 @@ theater *theater_create()
     return t;
 }
 
-void add_movie(theater *t, movie *m) 
-{
+void add_movie(theater *t, movie *m) {
     if (t->size >= t->capacity - 1)
     {
         t->capacity++;
@@ -35,8 +32,7 @@ void add_movie(theater *t, movie *m)
     t->all_movies[t->size++] = m;
 }
 
-void destroy_theater(theater *t)
-{
+void destroy_theater(theater *t) {
     for (size_t i = 0; i < t->size; t++)
     {
         movie_destroy(t->all_movies[i]);
@@ -45,8 +41,7 @@ void destroy_theater(theater *t)
     free(t);
 }
 
-movie *get_movie(theater *t, size_t i)
-{
+movie *get_movie(theater *t, size_t i) {
     if (i >= t->size)
     {
         return NULL;
@@ -54,8 +49,7 @@ movie *get_movie(theater *t, size_t i)
     return t->all_movies[i];
 }
 
-size_t tickets_sold(theater *t)
-{
+size_t tickets_sold(theater *t) {
     // ??????
     return 0;
 }
--- 03_10_2021/movies.c
+++ 03_10_2021/movies.c
@@ -4,19 +4,16 @@
 
 #include "movies.h"
 
-struct movie
-{
+struct movie {
     char *name;
     size_t tickets_sold;
     size_t tickets_left;
 };
 
-movie *movie_create(char *t)
-{
+movie *movie_create(char *t) {
     movie *m = malloc(sizeof(movie));
     size_t sold = ((long)m % 100);
-    if (m != NULL)
-    {
+    if (m != NULL) {
         size_t t_len = strlen(t);
         m->name = malloc(sizeof(char) * (t_len + 1));
         for (size_t i = 0; i < t_len; i++)
@@ -31,10 +28,8 @@ movie *movie_create(char *t)
     return m;
 }
 
-void movie_print(movie *m)
-{
-    if (m == NULL)
-    {
+void movie_print(movie *m) {
+    if (m == NULL) {
         fprintf(stderr, "INVALID MOVIE TO PRINT\n");
     }
     fprintf(stdout, "TITLE: %60s\t\t", m->name);
@@ -42,35 +37,29 @@ void movie_print(movie *m)
     fprintf(stdout, "TICKETS LEFT: %lu\n", m->tickets_left);
 }
 
-size_t get_sold(movie *m)
-{
-    if (m == NULL)
-    {
+size_t get_sold(movie *m) {
+    if (m == NULL) {
         return 0;
     }
     return m->tickets_sold;
 }
 
-size_t get_left(movie *m)
-{
-    if (m == NULL)
-    {
+size_t get_left(movie *m) {
+    if (m == NULL) {
         return 0;
     }
     return m->tickets_left;
 }
 
-char *get_name(movie *m)
-{
-    if (m == NULL)
-    {
+char *get_name(movie *m) {
+
+    if (m == NULL) {
         return NULL;
     }
     return m->name;
 }
 
-void movie_destroy(movie *m)
-{
+void movie_destroy(movie *m) {
     free(m->name);
     free(m);
 }
